de6147fdf1e84c657925b4ced073469f9d39941d
Maks <nordmax69@gmail.com>
2022-09-15 00:21:06 +0700

    added 1 homework

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
-Console.WriteLine("Hello, World!");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+void Recurse(int n,int m)
+{
+    if (m < n) return;
+    Recurse(n,m-1);
+    Console.Write("{0} ",m);
+}
+
+
+Recurse(5,50);
